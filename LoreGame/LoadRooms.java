11fbc8da4399af9ea2b37999bdd29f27832d3a46
Initech9 <ag9876@gmail.com>
2024-06-18 21:51:25 -0400

    North walls can be loaded from a text file now

--- LoreGame/LoadRooms.java
+++ LoreGame/LoadRooms.java
@@ -10,27 +10,21 @@ public class LoadRooms {
 
     MapReader map = new MapReader();
 
-int x;
-int y;
+    int x;
+    int y;
 
-public static int MAP_X_WIDTH = 4;
-public static int MAP_Y_HEIGHT = 4;
+    public static int MAP_X_WIDTH = 4;
+    public static int MAP_Y_HEIGHT = 4;
 
     Room room[][] = new Room[MAP_X_WIDTH][MAP_Y_HEIGHT];
-    
 
- 
+    //public BufferedImage northWall;
 
-    public BufferedImage northWall;
+    ReadNorthWalls northWall = new ReadNorthWalls();
 
-    public LoadRooms(Room room[][]){
+    public LoadRooms(Room room[][]) {
 
-   
-
-
-
-
-//map.printGrid("map.txt");
+        // map.printGrid("map.txt");
 
         // room[0][0] = new Room();
         // room[0][1] = new Room();
@@ -42,34 +36,35 @@ public static int MAP_Y_HEIGHT = 4;
         // room[2][1] = new Room();
         // room[2][2] = new Room();
 
-//this creates a new room on x, y and a collision detection if the Room is walkable
-        for(int x = 0; x < MAP_X_WIDTH; x++){
+        // this creates a new room on x, y and a collision detection if the Room is
+        // walkable
+        for (int x = 0; x < MAP_X_WIDTH; x++) {
             room[x][y] = new Room();
             room[x][y].walkable = map.grid[x][y];
-            
-            for(int y = 0; y < MAP_Y_HEIGHT; y++){
+
+            for (int y = 0; y < MAP_Y_HEIGHT; y++) {
                 room[x][y] = new Room();
                 room[x][y].walkable = map.grid[x][y];
             }
 
-            }
+        }
 
+        for(int x = 0; x < MAP_X_WIDTH; x++){
+        room[x][y].northWall = northWall.grid[y][x];
+        for(int y = 0; y < MAP_Y_HEIGHT; y++){
+        room[x][y].northWall = northWall.grid[y][x];
+        }
 
+        // room[0][0].northWall = 7;
+        // room[0][1].northWall = 0;
+        // room[0][2].northWall = 0;
+        // room[1][0].northWall = 8;
+        // room[1][1].northWall = 0;
+        // room[1][2].northWall = 0;
+        // room[2][0].northWall = 9;
+        // room[2][1].northWall = 0;
+        // room[2][2].northWall = 0;
 
-
-        room[0][0].northWall = 7;
-        room[0][1].northWall = 0;
-        room[0][2].northWall = 0;
-        room[1][0].northWall = 8;
-        room[1][1].northWall = 0;
-        room[1][2].northWall = 0;
-        room[2][0].northWall = 9;
-        room[2][1].northWall = 0;
-        room[2][2].northWall = 0;
-
-
-
-  
         room[0][0].eastWall = 0;
         room[0][1].eastWall = 0;
         room[0][2].eastWall = 0;
@@ -79,7 +74,7 @@ public static int MAP_Y_HEIGHT = 4;
         room[2][0].eastWall = 1;
         room[2][1].eastWall = 2;
         room[2][2].eastWall = 3;
-  
+
         room[0][0].westWall = 6;
         room[0][1].westWall = 5;
         room[0][2].westWall = 4;
@@ -89,7 +84,7 @@ public static int MAP_Y_HEIGHT = 4;
         room[2][0].westWall = 0;
         room[2][1].westWall = 0;
         room[2][2].westWall = 0;
-  
+
         room[0][0].southWall = 0;
         room[0][1].southWall = 0;
         room[0][2].southWall = 6;
@@ -110,22 +105,19 @@ public static int MAP_Y_HEIGHT = 4;
         room[2][1].floor = 9;
         room[2][2].floor = 9;
 
-
-        //Collision detection, tells if a room is walkable 1 if yes, 0 if no
+        // Collision detection, tells if a room is walkable 1 if yes, 0 if no
         // for(int x = 0; x < MAP_X_WIDTH; x++){
-        //     room[x][y].walkable = map.grid[x][y];
-        //     for(int y = 0; y < MAP_Y_HEIGHT; y++){
-        //         room[x][y].walkable = map.grid[x][y];
-        //     }
+        // room[x][y].walkable = map.grid[x][y];
+        // for(int y = 0; y < MAP_Y_HEIGHT; y++){
+        // room[x][y].walkable = map.grid[x][y];
+        // }
 
-        //     }
-//room[1][1].walkable = false;
+        // }
+        // room[1][1].walkable = false;
 
-//room[2][2].walkable = false;
+        // room[2][2].walkable = false;
 
-}
+    }
 
-
-
-    
+    }
 }
